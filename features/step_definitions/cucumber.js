9b154377980121c29be1ef319e8f79b04545a498
vj333v <128939515+vj333v@users.noreply.github.com>
2024-11-06 15:11:36 +0100

    Delete features/step_definitions directory

--- features/step_definitions/cucumber.js
+++
@@ -1,9 +0,0 @@
-// cucumber.js
-module.exports = {
-  default: {
-    require: ["features/step_definitions/**/*.js"],
-    format: ["pretty"],
-    paths: ["features/**/*.feature"],
-    publishQuiet: true,
-  },
-};
